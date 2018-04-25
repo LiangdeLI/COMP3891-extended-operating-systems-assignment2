5fc03ef76a5b115c0f32df0f9a1b16fd34f81021
z5077890 <z5077890@cse.unsw.edu.au>
2018-04-25 17:50:29 +1000

    Add some codes but not work yet

--- kern/syscall/file.c
+++ kern/syscall/file.c
@@ -19,3 +19,36 @@
  * Add your file-related functions here ...
  */
 
+
+
+//Add************************** The below code part still not work yet...
+//int open(const char *filename, int flags, ...);
+int s_open(const_userptr_t file, int flag, mode_t mode, int32_t retVal){
+	
+	int fd = 3;
+	int res;
+	//if the file is NULL, there is no such file.
+	if(file == NULL){
+		return ENOENT;
+	}
+
+	curthread->t_fdtable[fd] = kmalloc(sizeof(struct fdesc));
+	
+	//if the file decriptor table is null, it means: "Bad memory reference"?
+	if(curthread->t_fdtable[fd] == NULL){
+		return EFAULT;
+	}
+	
+	struct vnode* v = NULL;
+	
+	// try to call vfs_open to open the file
+	if (res = vfs_open((char*) file, flag, mode, &v)){
+		return res
+	}
+
+	return 0;
+}
+
+
+
+//****************************
